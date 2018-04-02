1ad800c308b04e75d99f0d8289a5181500c301e5
Whyle <yihao.zhuo.2001@gmail.com>
2018-04-02 20:13:43 +0200

    test

---
+++ events/20180425/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
